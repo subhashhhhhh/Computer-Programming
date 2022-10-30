cf85bc4093e5e4ed64ea6beb838b22fb9d1a7f1b
Subhash Gottumukkala <gsubhash1234567@gmail.com>
2022-10-30 15:10:44 +0530

    Update 34022_subhash_Q7.c

--- Diwali Assignment/34022_subhash_Q7.c
+++ Diwali Assignment/34022_subhash_Q7.c
@@ -1,50 +1,54 @@
 #include<stdio.h>
-void smith(int n)
-{
-    int a,b=0,c=0,d,e,f,g,h;
-    a=n;
-    while(a>0)
-    {
-        b=b+(a%10);
-        a/=10;
-    }
-    a=n;
-    while(a>1)
-    {
-        for(d=2;d<=a;d++)
-        {
-            if(a%d==0)
-            break;
-        }
-        h=0;
-        e=d;
-        for(g=1;g<=d;g++)
-        {
-            if(d%g==0)
-            h++;
-        }
-        if(h==2)
-        {
-            while(d>0)
-            {
-                c=c+(d%10);
-                d=d/10;
-            }
-        }
-        a=a/e;
-
-    }
-    if(b==c)
-    printf("\nThe given number is a smith number");
-    else
-    printf("\nThe given number is not a smith number");
-}
-
 int main()
 {
-    int n;
-    printf("Subhash Gottumukkala (34022");
-    printf("Enter the number: ");
-    scanf("%d", &n);
-    smith(n);
+	printf("Subhash Gottumukkala (34022\n");
+int a,b,sumd=0,sump=0,r,c; 
+printf("Enter a number");
+scanf("%d",&a);
+b=a;
+while(a>0||sumd>9)
+{
+if(a==0)
+{
+a=sumd;
+sumd=0;
+}
+r=a%10;
+sumd=sumd+r;
+a=a/10;
+}
+a=b;
+c=2;
+while(a>1)
+{
+while(a%c==0)
+{
+printf("%d\b",c);
+sump=sump+c;
+a=a/c;
+}
+c=c+1;
+if(c>b/2)
+break;
+}
+
+a=sump;
+sump=0;
+while(a>0||sump>9)
+{
+if(a==0)
+{
+a=sump;
+sump=0;
+}
+r=a%10;
+sump=sump+r;
+a=a/10;
+}
+if(sumd==sump)
+printf("\n %d is a Smith number",b);
+else
+printf("%d is not a Smith number",b);
+
+return 0;
 }
