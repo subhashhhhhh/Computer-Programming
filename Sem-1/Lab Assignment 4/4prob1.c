e56525f6f4db8b9cff2f2836bb1e105b828b415d
Subhash Gottumukkala <gsubhash1234567@gmail.com>
2022-11-04 19:35:59 +0530

    Create 4prob1.c

---
+++ Sem-1/Lab Assignment 4/4prob1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    float b;
+    double c;
+    char d;
+    printf("Size of int: %d bytes\n", sizeof(a));
+    printf("Size of float: %d bytes\n", sizeof(b));
+    printf("Size of double: %d bytes\n", sizeof(c));
+    printf("Size of char: %d byte\n", sizeof(d));
+    return 0;
+}
