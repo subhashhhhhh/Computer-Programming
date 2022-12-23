0b8c41a77bca33c9050323dcb6e4a3e20f2da93a
Subhash Gottumukkala <79552953+subhashgottumukkala@users.noreply.github.com>
2022-12-23 15:10:27 +0530

    Create fibonacci.c

---
+++ Sem-1/Lab Assignment 6/fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+  int num, i;
+  long long int t1 = 0, t2 = 1, nextTerm;
+
+  printf("Enter the number of terms: ");
+  scanf("%d", &num);
+
+  printf("Fibonacci Series: ");
+
+  for (i = 1; i <= num; ++i) {
+    printf("%lld, ", t1);
+    nextTerm = t1 + t2;
+    t1 = t2;
+    t2 = nextTerm;
+  }
+  
+  return 0;
+}
