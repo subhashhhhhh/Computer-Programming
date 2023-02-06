0da17358e213af2c4375d83d6a58592da300b532
subhash <subhashgottumukkala17@gmail.com>
2023-02-06 23:53:16 +0530

    updated prob5.cpp

--- Sem-2/Lab Assignment 1/prob5.cpp
+++ Sem-2/Lab Assignment 1/prob5.cpp
@@ -24,5 +24,40 @@ void addDetails(){
 }
 
 void updateDetails(){
-    
+    cout << "Enter a new name";
+    cin >> name;
+    cout << "Enter a new Roll no";
+    cin >> rollNo;
+    cout << "Enter a new degree";
+    cin >> degree;
+}
+
+void updateCGPA(){
+    cout << "Enter a new CGPA";
+    cin >> currentCGPA;
+}
+
+void updateHostel(){
+    cout << "Enter a new hostel";
+    cin >> hostel;
+}
+
+void displayDetails(){
+    cout << "Name: " << name << endl;
+    cout << "Roll no: " << rollNo << endl;
+    cout << "Degree: " << degree << endl;
+    cout << "Hostel: " << hostel << endl;
+    cout << "Current CGPA: " << currentCGPA << endl;
+}
+
+int main() {
+  Student s;
+  s.addDetails();
+  std::cout << "Student details:" << std::endl;
+  s.displayDetails();
+  s.updateDetails();
+  std::cout << "Updated student details:" << std::endl;
+  s.displayDetails();
+
+  return 0;
 }
